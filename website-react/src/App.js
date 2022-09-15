8439a67fc2377ef034b03d3749165d69a205452a
Pedro Santayana <pedrosantayana@gmail.com>
2022-09-15 12:40:51 -0300

    react init

---
+++ website-react/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import Button from 'react-bootstrap/Button';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+          <Button href='https://doc.rust-lang.org/book/ch03-03-how-functions-work.html'>Learn Rust</Button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
