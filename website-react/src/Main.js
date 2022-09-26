b5e3a37ca1426628ebb5342c3c53b49e339397a6
Pedro Santayana <pedrosantayana@gmail.com>
2022-09-26 10:29:34 -0300

    add login route

--- website-react/src/Main.js
+++ website-react/src/Main.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 
 import Home from './pages/Home';
+import Login from './pages/Login';
 
 const Main = () => {
   return (
 <BrowserRouter>
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
     </Routes>
   </BrowserRouter>
   );
