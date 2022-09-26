6b827518dcb5978b1a30b086c90de6cd4bfbf0a6
Pedro Santayana <pedrosantayana@gmail.com>
2022-09-26 09:25:23 -0300

    init header (navbar) component

---
+++ website-react/src/components/Header.jsx
@@ -0,0 +1,46 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import React from 'react';
+
+function Header() {
+  return (
+    <Navbar bg="warning" expand="lg">
+      <Container>
+        <Navbar.Brand href="#home">
+          <img
+            src="/logo.svg"
+            width="30"
+            height="30"
+            className="d-inline-block align-top"
+            alt="Facebug logo"
+          />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Assuntos do Momento</Nav.Link>
+            <Nav.Link href="#link">Problemas, Ajude!</Nav.Link>
+            <Nav.Link href="#link">Curtidas</Nav.Link>
+            <Nav.Link href="#link">Publicar Post</Nav.Link>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default Header;
