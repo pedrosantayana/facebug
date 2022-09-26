5c7f29ac8fe3ddf911c0198973f351023c8f1579
Pedro Santayana <pedrosantayana@gmail.com>
2022-09-26 10:03:52 -0300

    feat: routes configured

--- website-react/src/App.js
+++ website-react/src/App.js
@@ -1,18 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
-import Button from 'react-bootstrap/Button';
 import React from 'react';
+import Header from './components/Header';
+import Main from './Main';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-          <Button href='https://doc.rust-lang.org/book/ch03-03-how-functions-work.html'>Learn Rust</Button>
-      </header>
+      <Header />
+      <Main />
     </div>
   );
 }
