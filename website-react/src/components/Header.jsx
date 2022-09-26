869887049d1dea093bd05bc328b82f4e70b7ef5b
Pedro Santayana <pedrosantayana@gmail.com>
2022-09-26 10:28:37 -0300

    todo: navbar to finish

--- website-react/src/components/Header.jsx
+++ website-react/src/components/Header.jsx
@@ -3,14 +3,16 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import React from 'react';
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/esm/Button';
 
 function Header() {
   return (
     <Navbar bg="warning" expand="lg">
       <Container>
-        <Navbar.Brand href="#home">
+        <Navbar.Brand href="/">
           <img
-            src="/logo.svg"
+            src="/logo.png"
             width="30"
             height="30"
             className="d-inline-block align-top"
@@ -20,11 +22,14 @@ function Header() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Assuntos do Momento</Nav.Link>
-            <Nav.Link href="#link">Problemas, Ajude!</Nav.Link>
-            <Nav.Link href="#link">Curtidas</Nav.Link>
-            <Nav.Link href="#link">Publicar Post</Nav.Link>
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/trends">Assuntos do Momento</Nav.Link>
+            <Nav.Link href="/problems">Problemas, Ajude!</Nav.Link>
+            <Nav.Link href="/likes">Curtidas</Nav.Link>
+            <Nav.Link href="/newpost">Publicar Post</Nav.Link>
+            <Nav.Link href="/account">
+              <img src="" alt="" />
+            </Nav.Link>
             <NavDropdown title="Dropdown" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
@@ -37,6 +42,15 @@ function Header() {
               </NavDropdown.Item>
             </NavDropdown>
           </Nav>
+          <Form className="d-flex">
+            <Form.Control
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="outline-success">Search</Button>
+          </Form>
         </Navbar.Collapse>
       </Container>
     </Navbar>
