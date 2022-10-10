725f0857e6796ad2d3e8d64f44f6a07fcfb23330
Pedro Santayana <pedrosantayana@gmail.com>
2022-10-10 13:09:48 -0300

    fix: routes

--- backend/src/main/java/app/Facebug.java
+++ backend/src/main/java/app/Facebug.java
@@ -2,39 +2,40 @@ package app;
 
 import static spark.Spark.*;
 
+import service.CategoriaService;
 import service.PostagemService;
-import service.ProdutoService;
 import service.UsuarioService;
 
 public class Facebug {
-  private static ProdutoService produtoService = new ProdutoService();
   private static UsuarioService usuarioService = new UsuarioService();
   private static PostagemService postagemService = new PostagemService();
-  private static FeedService feedService = new FeedService();
+  private static CategoriaService categoriaService = new CategoriaService();
+
   public static void main(String[] args) {
     port(6789);
 
     staticFiles.location("/public");
 
-    post("/usuario/cadastrar", (request, response) -> UsuarioService.register(request, response));
-    post("/usuario/login", (request, response) -> UsuarioService.login(requst, response));
-    post("/usuario/update", (request, response) -> UsuarioService.update(request, response));
-    post("/usuario/delete", (request, response) -> UsuarioService.delete(request, response));
-    get("/usuario/info/:username", (request, response) -> UsuarioService.get(request, response));
-    get("/usuario/pesquisar/:username", (request, response) -> UsuarioService.search(request, response));
-    get("/usuario/feed/:username", (request, response) -> UsuarioService.feed(request, response));
+    post("/usuario/cadastrar", (request, response) -> usuarioService.register(request, response));
+    post("/usuario/login", (request, response) -> usuarioService.login(request, response));
+    post("/usuario/update", (request, response) -> usuarioService.update(request, response));
+    post("/usuario/delete", (request, response) -> usuarioService.delete(request, response));
+    get("/usuario/info/:username", (request, response) -> usuarioService.get(request, response));
+    get("/usuario/pesquisar/:username", (request, response) -> usuarioService.search(request, response));
 
-    post("/postagem/criar", (request, response) -> PostagemService.insert(request, response));
-    post("/postagem/update", (request, response) -> PostagemService.update(request, response));
-    post("/postagem/delete", (request, response) -> PostagemService.delete(request, response));
-    get("/postagem/:id", (request, response) -> PostagemService.get(request, response));
-    get("/postagem/listar/categoria/:id", (request, response) -> PostagemService.list(request, response));
-    get("/postagem/usuario/:username", (request, response) -> PostagemService.list(request, response));
+    // Gera o feed com postagens relevantes ao usuario
+    get("/usuario/feed/:username", (request, response) -> usuarioService.feed(request, response));
 
-    
-    get("/feed/:username", (request, response) -> );
+    post("/postagem/criar", (request, response) -> postagemService.insert(request, response));
+    post("/postagem/update", (request, response) -> postagemService.update(request, response));
+    post("/postagem/delete", (request, response) -> postagemService.delete(request, response));
+    get("/postagem/:id", (request, response) -> postagemService.get(request, response));
+    get("/postagem/listar/categoria/:id", (request, response) -> postagemService.list(request, response));
+    get("/postagem/usuario/:username", (request, response) -> postagemService.list(request, response));
 
-    get("/categoria/listar", (request, response) -> );
+    get("/categoria/listar", (request, response) -> categoriaService.list(request, response));
+
+    // get("/feed/:username", (request, response) -> );
 
   }
 }
