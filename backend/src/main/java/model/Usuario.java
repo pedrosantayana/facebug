828469c94c4ebe2ccd97db445bf177fab15f65b2
Pedro Santayana <pedrosantayana@gmail.com>
2022-10-10 13:25:37 -0300

    feat: usuario getters and setters

--- backend/src/main/java/model/Usuario.java
+++ backend/src/main/java/model/Usuario.java
@@ -8,4 +8,28 @@ public class Usuario {
   public Usuario() {
     
   }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public byte[] getHashedPassword() {
+    return hashedPassword;
+  }
+
+  public void setHashedPassword(byte[] hashedPassword) {
+    this.hashedPassword = hashedPassword;
+  }
 }
