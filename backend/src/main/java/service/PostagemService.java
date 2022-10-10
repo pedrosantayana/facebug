58695eb65e9c666bb4f93d061e68bbb9c78c3738
Pedro Santayana <pedrosantayana@gmail.com>
2022-10-10 13:47:59 -0300

    todo: postagem service routes

--- backend/src/main/java/service/PostagemService.java
+++ backend/src/main/java/service/PostagemService.java
@@ -1,6 +1,35 @@
 package service;
 
+import dao.PostagemDAO;
+import spark.Request;
+import spark.Response;
+
 public class PostagemService {
-  private PostagemDAO 
+  private PostagemDAO postagemDAO = new PostagemDAO();
+
+  public PostagemService() {
+
+  }
+
+  public Object insert(Request request, Response response) {
+    return null;
+  }
+
+  public Object update(Request request, Response response) {
+    return null;
+  }
+
+  public Object delete(Request request, Response response) {
+    return null;
+  }
+
+  public Object get(Request request, Response response) {
+    return null;
+  }
+
+  public Object list(Request request, Response response) {
+    return null;
+  }
+
 
 }
