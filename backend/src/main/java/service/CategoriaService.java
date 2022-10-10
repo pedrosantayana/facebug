7dcf04422caa98dcf1066e49dd5576f219786d31
Pedro Santayana <pedrosantayana@gmail.com>
2022-10-10 13:28:05 -0300

    todo: função para retornar as categorias em json

--- backend/src/main/java/service/CategoriaService.java
+++ backend/src/main/java/service/CategoriaService.java
@@ -1,8 +1,15 @@
 package service;
 
+import spark.Request;
+import spark.Response;
+
 public class CategoriaService {
 
   public CategoriaService() {
+  }
+
+  public Object list(Request request, Response response) {
+    return null;
   }
   
 }
