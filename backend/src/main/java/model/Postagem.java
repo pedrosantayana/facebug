5a8e36bc0b8a5258b4b5fbd4d8b216e0902c730d
Pedro Santayana <pedrosantayana@gmail.com>
2022-10-11 16:00:34 -0300

    fix: postagem class finished

--- backend/src/main/java/model/Postagem.java
+++ backend/src/main/java/model/Postagem.java
@@ -3,67 +3,95 @@ package model;
 import java.sql.Date;
 import java.util.UUID;
 
+import org.json.JSONObject;
+
 public class Postagem {
   private UUID id;
-  private Usuario user;
+  private String title;
   private String content;
   private String media;
-  private Categoria category;
-  private String title;
+  private UUID categoryId;
   private Date date;
+  private String ownerUsername;
 
-  public Postagem(UUID id, Usuario user, String content, String media, Categoria category, String title, Date date) {
+  public Postagem(UUID id, String title, String content, String media, UUID categoryId, Date date,
+      String ownerUsername) {
     this.id = id;
-    this.user = user;
+    this.title = title;
     this.content = content;
     this.media = media;
-    this.category = category;
-    this.title = title;
+    this.categoryId = categoryId;
     this.date = date;
+    this.ownerUsername = ownerUsername;
   }
-  public Postagem() {
-    
+
+  public JSONObject toJSON() {
+    JSONObject resp = new JSONObject();
+
+    resp.put("id", id.toString());
+    resp.put("title", title);
+    resp.put("content", content);
+    resp.put("media", media);
+    resp.put("categoryId", categoryId.toString());
+    resp.put("date", date.getTime());
+    resp.put("ownerUsername", ownerUsername);
+
+    return resp;
   }
-  public Usuario getUser() {
-    return user;
-  }
-  public void setUser(Usuario user) {
-    this.user = user;
-  }
+
   public String getContent() {
     return content;
   }
+
   public void setContent(String content) {
     this.content = content;
   }
+
   public String getMedia() {
     return media;
   }
+
   public void setMedia(String media) {
     this.media = media;
   }
-  public Categoria getCategory() {
-    return category;
-  }
-  public void setCategory(Categoria category) {
-    this.category = category;
-  }
+
   public String getTitle() {
     return title;
   }
+
   public void setTitle(String title) {
     this.title = title;
   }
+
   public Date getDate() {
     return date;
   }
+
   public void setDate(Date date) {
     this.date = date;
   }
+
   public UUID getId() {
     return id;
   }
+
   public void setId(UUID id) {
     this.id = id;
+  }
+
+  public UUID getCategoryId() {
+    return categoryId;
+  }
+
+  public void setCategoryId(UUID categoryId) {
+    this.categoryId = categoryId;
+  }
+
+  public String getOwnerUsername() {
+    return ownerUsername;
+  }
+
+  public void setOwnerUsername(String ownerUsername) {
+    this.ownerUsername = ownerUsername;
   }
 }
