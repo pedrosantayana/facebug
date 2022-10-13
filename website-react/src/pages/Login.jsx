72ea951d531c306c85b5216f00c4d25e6eabdc0e
Pedro Santayana <pedrosantayana@gmail.com>
2022-10-13 20:43:26 -0300

    feat: login form handler

--- website-react/src/pages/Login.jsx
+++ website-react/src/pages/Login.jsx
@@ -2,11 +2,23 @@ import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/esm/Container';
 import Form from 'react-bootstrap/Form';
-
+import { login } from '../api/facebug';
 function Login() {
+  state = {
+    username: null,
+    password: null
+  }
   const fn = (event) => {
     event.preventDefault();
-    // const username = event.
+    login(this.state.username, this.state.password)
+      .then(res => {
+        if(res.status == 0) {
+          localStorage.setItem("session", JSON.stringify(res.session));
+          // redirect to home
+        } else {
+          console.log("Não foi possível logar. Status: "+res.status);
+        }
+      });
   }
 
 
@@ -16,12 +28,12 @@ function Login() {
       <Form onSubmit={fn}>
         <Form.Group className="mb-3" controlId="username">
           <Form.Label>Usuário</Form.Label>
-          <Form.Control id="username" type="text" placeholder="Nome de Usuário" />
+          <Form.Control onChange={e => this.setState({username: e.target.value, password: this.state.password})} type="text" placeholder="Nome de Usuário" />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="password">
           <Form.Label>Senha</Form.Label>
-          <Form.Control id="password" type="password" placeholder="Senha" />
+          <Form.Control onChange={e => this.setState({username: this.state.username, password: e.target.value})} type="password" placeholder="Senha" />
         </Form.Group>
         <Button variant="primary" type="submit">
           Login
