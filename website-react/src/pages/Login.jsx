ed25bd3a5d57e0b1906898f746edd377154ddd62
Pedro Santayana <pedrosantayana@gmail.com>
2022-10-13 20:54:49 -0300

    declare state

--- website-react/src/pages/Login.jsx
+++ website-react/src/pages/Login.jsx
@@ -4,13 +4,12 @@ import Container from 'react-bootstrap/esm/Container';
 import Form from 'react-bootstrap/Form';
 import { login } from '../api/facebug';
 function Login() {
-  state = {
-    username: null,
-    password: null
-  }
+  const [username, setUsername] = useState();
+  const [password, setPassword] = useState();
+
   const fn = (event) => {
     event.preventDefault();
-    login(this.state.username, this.state.password)
+    login(username, password)
       .then(res => {
         if(res.status == 0) {
           localStorage.setItem("session", JSON.stringify(res.session));
@@ -28,12 +27,12 @@ function Login() {
       <Form onSubmit={fn}>
         <Form.Group className="mb-3" controlId="username">
           <Form.Label>Usuário</Form.Label>
-          <Form.Control onChange={e => this.setState({username: e.target.value, password: this.state.password})} type="text" placeholder="Nome de Usuário" />
+          <Form.Control onChange={e => setUsername(e.target.value)} type="text" placeholder="Nome de Usuário" />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="password">
           <Form.Label>Senha</Form.Label>
-          <Form.Control onChange={e => this.setState({username: this.state.username, password: e.target.value})} type="password" placeholder="Senha" />
+          <Form.Control onChange={e => setPassword(e.target.password)} type="password" placeholder="Senha" />
         </Form.Group>
         <Button variant="primary" type="submit">
           Login
