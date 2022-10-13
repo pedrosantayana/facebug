8e74cc0d62a49afe9db07a4a1df72f6a979b1f2b
Pedro Santayana <pedrosantayana@gmail.com>
2022-10-13 18:26:48 -0300

    new routes

--- website-react/src/Main.js
+++ website-react/src/Main.js
@@ -10,18 +10,20 @@ import Problems from './pages/Problems';
 import Likes from './pages/Likes';
 
 const Main = () => {
+  const isLoggedin = localStorage.getItem("session");
+
   return (
-<BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/trends" element={<Trends />} />
-      <Route path="/profile" element={<Profile />}/>
-      <Route path="/newPost" element={<NewPost />}/>
-      <Route path="/problems" element={<Problems />}/>
-      <Route path="/likes" element={<Likes />}/>
-    </Routes>
-  </BrowserRouter>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={isLoggedin ? <Home /> : <Login />} />
+        <Route path="/login" element={isLoggedin ? <Home /> : <Login />} />
+        <Route path="/trends" element={isLoggedin ? <Trends /> : <Login />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/newPost" element={isLoggedin ? <NewPost /> : <Login />} />
+        <Route path="/problems" element={isLoggedin ? <Problems /> : <Login />} />
+        <Route path="/likes" element={<Likes />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
