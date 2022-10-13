948acabbc0c9fcd421c2c1e52fb6e5302fb3dae0
Pedro Santayana <pedrosantayana@gmail.com>
2022-10-13 19:47:38 -0300

    fix: remove redirect tag

--- website-react/src/pages/Home.jsx
+++ website-react/src/pages/Home.jsx
@@ -1,15 +1,9 @@
 import React from "react"
 import Post from '../components/Post'
-import { Redirect } from 'react-router-dom'
 
 export default () => {
   const session = JSON.parse(localStorage.getItem("session"));
-  if (session) {
-    
-    return (
-      <Post category="Categoria" title="Qualquer titulo ai" author="Fabio Freire" description="descricao" />
-    )
-  } else {
-    return (<Redirect to="/login" />);
-  }
+  return (
+    <Post category="Categoria" title="Qualquer titulo ai" author="Fabio Freire" description="descricao" />
+  )
 }
