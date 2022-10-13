830b0e0e53630e577d32408d92e6253d92214862
Pedro Santayana <pedrosantayana@gmail.com>
2022-10-13 20:30:08 -0300

    todo: handle onsubmit event

--- website-react/src/pages/Login.jsx
+++ website-react/src/pages/Login.jsx
@@ -4,18 +4,24 @@ import Container from 'react-bootstrap/esm/Container';
 import Form from 'react-bootstrap/Form';
 
 function Login() {
+  const fn = (event) => {
+    event.preventDefault();
+    // const username = event.
+  }
+
+
   return (
     <Container fluid class="bg-warning">
       <h1>Login</h1>
-      <Form>
+      <Form onSubmit={fn}>
         <Form.Group className="mb-3" controlId="username">
           <Form.Label>Usuário</Form.Label>
-          <Form.Control type="text" placeholder="Nome de Usuário" />
+          <Form.Control id="username" type="text" placeholder="Nome de Usuário" />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="password">
           <Form.Label>Senha</Form.Label>
-          <Form.Control type="password" placeholder="Senha" />
+          <Form.Control id="password" type="password" placeholder="Senha" />
         </Form.Group>
         <Button variant="primary" type="submit">
           Login
