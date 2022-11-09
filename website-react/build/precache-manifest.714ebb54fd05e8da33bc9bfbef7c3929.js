2fd6af8a866ccb943b9f19e560db2bf31fbe207a
Fábio Freire Kochem <97361731+fabiokochem@users.noreply.github.com>
2022-11-09 14:59:17 -0300

    altered

---
+++ website-react/build/precache-manifest.714ebb54fd05e8da33bc9bfbef7c3929.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "a825d962737bf17d476a",
+    "url": "/static/css/main.e65b5e82.chunk.css"
+  },
+  {
+    "revision": "a825d962737bf17d476a",
+    "url": "/static/js/main.a825d962.chunk.js"
+  },
+  {
+    "revision": "462fada37566201ee509",
+    "url": "/static/js/1.462fada3.chunk.js"
+  },
+  {
+    "revision": "4ea62ea0ba37dadf1fa5",
+    "url": "/static/css/2.0abfe925.chunk.css"
+  },
+  {
+    "revision": "4ea62ea0ba37dadf1fa5",
+    "url": "/static/js/2.4ea62ea0.chunk.js"
+  },
+  {
+    "revision": "6d3d03d1a2b188b3ec90",
+    "url": "/static/js/runtime~main.6d3d03d1.js"
+  },
+  {
+    "revision": "f86ac30b469339897fb0f1e6b8ddd883",
+    "url": "/index.html"
+  }
+];
