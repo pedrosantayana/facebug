6ad36b7fff4300dd9cbeafeac71995201dab829b
Pedro Santayana <pedrosantayana@gmail.com>
2022-11-11 13:01:45 -0300

    fix: removed all warnings

--- website/src/App.js
+++ website/src/App.js
@@ -3,7 +3,6 @@ import React from 'react';
 import Header from './components/Header';
 import Main from './Main';
 import Footer from './components/Footer'
-import Home from './pages/Home'
 
 function App() {
   return (
--- website/src/components/Post.jsx
+++ website/src/components/Post.jsx
@@ -15,19 +15,19 @@ function Post(props) {
           <div className='card-body'>
             <h4 className='card-title text-primary text-center'>{props.category}</h4>
             <h5 className='card-title'>{props.title}</h5>
-            <a href='#' className='text-decoration-none'>
+            <a href={'/profile/'+props.author.username} className='text-decoration-none'>
               <h6 className='text-warning'>{props.author}</h6>
             </a>
             <button>SEGUIR</button>
             <p className='card-text'>{props.description}</p>
           </div>
-          <img className='card-img-top p-2' src='img/img1.jpeg' alt='Card image cap'></img>
-          <a href="#" className='text-center p-1 text-danger' style={styles.style1}>
+          <img className='card-img-top p-2' src='img/img1.jpeg' alt=''></img>
+          <p className='text-center p-1 text-danger' style={styles.style1}>
             <i className='fa-solid fa-heart'></i>
-          </a>
-          <a href='#' className='text-center p-1 text-success' style={styles.style1}>
+          </p>
+          <p className='text-center p-1 text-success' style={styles.style1}>
             <i className='fa-solid fa-comments'></i>
-          </a>
+          </p>
         </Card>
       </div>
     </div>
